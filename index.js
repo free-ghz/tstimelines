db9a854b8070112edf6a36fc250772e9afbdc69e
free-ghz <elmusho@gmail.com>
2024-02-05 21:59:13 +0100

    Stream completions

--- index.js
+++ index.js
@@ -20,6 +20,42 @@ async function complete(text, tokens) {
     return res
 }
 
+
+function completeStream(text, tokens) {
+    let url = baseUrl + "/engines/" + engine + "/completions"
+    let request = {
+        prompt: text,
+        max_tokens: tokens,
+        temperature: 1,
+        top_k: 40,
+        top_p: 1,
+        stream: true
+    }
+    let stream = got.stream.post(url, {
+        json: request
+    })
+    let completeText = ""
+    return new Promise((resolve, reject) => {
+        let handleJson = (json) => {
+            if (json == '') return
+            let {text, reached_end, finish_reason} = JSON.parse(json)
+            completeText += text
+            process.stdout.write(text)
+            if (reached_end) {
+                resolve({
+                        finish_reason: finish_reason,
+                        text: completeText
+                })
+            }
+        }
+        stream.on("data", async res => {
+            let incoming = Buffer.from(res).toString("utf-8")
+            let inArray = incoming.split("\n\n")
+            inArray.forEach(handleJson)
+        })
+    })
+}
+
 async function makeChat(chat, tokens) {
     let url = baseUrl + "/engines/" + engine + "/chat"
     let res = await got.post(url, {
@@ -39,14 +75,11 @@ async function makeChat(chat, tokens) {
 async function run() {
     let primer = await fs.readFile("primer.txt", 'utf8')
     let promptTemplate = await fs.readFile("prompt.txt", 'utf8')
-    console.log(">", primer)
-    let primerResponse = (await complete(primer, 300)).text
-    primerResponse = primer + primerResponse
-    console.log("<", primerResponse)
-    let prompt = promptTemplate.replace("€", primerResponse)
-    console.log(">", prompt)
-    let promptResponse = (await complete(prompt, 9999)).text
-    console.log("<", promptResponse)
+    let primerResponse = await completeStream(primer, 400)
+    console.log("\n\n∑   (Done.) reason:", primerResponse.finish_reason)
+    let prompt = promptTemplate.replace("€", primerResponse.text)
+    let promptResponse = (await completeStream(prompt, 9999))
+    console.log("\n\n∑   (Done.) reason:", promptResponse.finish_reason)
 }
 
 run()
